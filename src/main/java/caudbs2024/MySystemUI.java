7f78a44262e808ae233f489639f6ad8d0862b563
falconlee236 <falconlee236@cau.ac.kr>
2024-03-25 14:21:55 +0900

    move project

---
+++ src/main/java/caudbs2024/MySystemUI.java
@@ -0,0 +1,18 @@
+package caudbs2024;
+
+public class MySystemUI {
+    public void printMenu(){
+        System.out.println("<Menu>");
+        System.out.println("1. DB Create");
+        System.out.println("2. DB Insert");
+        System.out.println("3. DB Delete");
+        System.out.println("4. DB Select all");
+        System.out.println("5. DB Select one");
+        System.out.println("6. Exit");
+        System.out.print("Select the number: ");
+    }
+
+    public void createDB(){
+
+    }
+}
