3b19b310ac7c08aeaf300f5eb2f71d451d0d57a0
falconlee236 <falconlee236@cau.ac.kr>
2024-03-28 18:31:47 +0900

    add MyFileIOSystem.java

---
+++ src/main/java/caudbs2024/MyFileIOSystem.java
@@ -0,0 +1,9 @@
+package caudbs2024;
+
+public class MyFileIOSystem {
+    public void createDBFile(String relationName, Attribute[] attributes){
+        int attribute_num = attributes.length;
+        byte[] block = new byte[300];
+        byte[] buffer = new byte[100];
+    }
+}
