719248b4d6f08c7a498107309e183aa4a538fe6e
falconlee236 <falconlee236@cau.ac.kr>
2024-03-29 18:13:53 +0900

    start implement readDBFile function

--- src/main/java/caudbs2024/MyFileIOSystem.java
+++ src/main/java/caudbs2024/MyFileIOSystem.java
@@ -1,10 +1,9 @@
 package caudbs2024;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MyFileIOSystem {
@@ -75,7 +74,14 @@ public class MyFileIOSystem {
         }
     }
 
-    public void readDBFile(String relationName){
+    public void readDBFile(String relationName, Attribute[] attributes){
+        final int attribute_num = attributes.length;
+
+        try (FileInputStream fis = new FileInputStream(relationName + "txt")){
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
